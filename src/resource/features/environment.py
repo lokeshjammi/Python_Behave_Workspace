19949b5686916eb2922ce1a1991386fa19e2a785
LokeshJammi <jammilokesh3@gmail.com>
2022-09-09 12:23:55 -0700

    New code commit

--- src/resource/features/environment.py
+++ src/resource/features/environment.py
@@ -1,6 +1,14 @@
+import os
+from os.path import abspath, join
+
+from selenium import webdriver
+
+project_root = abspath(os.path.dirname(__file__))
+driver_bin = join(project_root, '../DriverExecutables/Windows/Edge/msedgedriver.exe')
 def before_scenario(context, feature):
     if 'UI' in feature.tags:
-        print('This comes first')
+        context.browser = webdriver.Edge(executable_path=driver_bin)
+        context.browser.maximize_window()
 
 def after_scenario(context, feature):
     if 'UI' in feature.tags:
